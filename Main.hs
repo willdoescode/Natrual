02523b53fb5e109b38d9aec66f42907d77564318
Will Lane <williamlane923@gmail.com>
2021-06-12 22:20:46 -0700

    .

--- Main.hs
+++ Main.hs
@@ -2,34 +2,12 @@ import Test.HUnit
 
 data Nat = Z | S Nat deriving (Eq)
 
-instance Show Nat where
-  show = show . natToInt
-
 natToInt :: Nat -> Int
 natToInt Z = 0
 natToInt (S k) = 1 + natToInt k
 
-intToNat :: Integer -> Nat
-intToNat 0 = Z
-intToNat x = S (intToNat (x - 1))
-
-e :: Nat -> Bool
-e Z = True
-e (S k) = o k
-  where
-    o :: Nat -> Bool
-    o Z = False
-    o (S k) = e k
-
--- instance Enum Nat
-
--- instance Eq Nat
-
--- instance Ord Nat
-
--- instance Real Nat
-
--- instance Integral Nat
+instance Show Nat where
+  show = show . natToInt
 
 instance Num Nat where
   fromInteger 0 = Z
